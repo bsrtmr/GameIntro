ff757b0ad27c04b211f865351b1dd726bd5479dd
Büşra Tamer <tamerbusraa@gmail.com>
2021-02-25 15:06:19 +0300

    Add project files.

---
+++ GameProjectDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GameProjectDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            //GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager()); //Hangi sistemi kullanmak istersek onu dahil ederiz.
+            gamerManager.Add(new Gamer
+            {
+                Id = 1,
+                BirthYear = 1992,
+                FirstName = "Büşra",
+                LastName = "Tamer",
+                IdentityNumber = 123456
+            });
+        }
+    }
+}
---
+++ GameProjectDemo/UserValidationManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProjectDemo
+{
+    public class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 1992 && gamer.FirstName == "Büşra" && gamer.LastName == "Tamer" && gamer.IdentityNumber == 123456)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
